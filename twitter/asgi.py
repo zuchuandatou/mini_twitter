9d0c4cf6f8389d2f802b4acf5bfff3cf7603f1fa
zuchuandatou <139428423+zuchuandatou@users.noreply.github.com>
2024-06-08 09:57:12 -0400

    Init project add accounts app

---
+++ twitter/asgi.py
@@ -0,0 +1,26 @@
+"""
+ASGI config for twitter project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twitter.settings')
+
+application = get_asgi_application()
+
+
+"""
+
++微信cuu888cuu
+九章算法 - 最新课程 - 全都有
+钓鱼网站千万别买 都是骗人的
+唯一课表：https://leaaiv.cn/jiuzhang
+
+"""
